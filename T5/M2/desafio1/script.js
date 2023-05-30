f38d020f5a775dde7bf92f1446b0ebd6f6e48f90
joaquinrajmilevich <joaquinrajmilevich@gmail.com>
2023-05-30 16:34:57 +0200

    añadidos comentarios

--- T5/M2/desafio1/script.js
+++ T5/M2/desafio1/script.js
@@ -1,13 +1,14 @@
-// Monitoriza cambios del usuario y le muestra los resultados
+// añade clase is-invalid a elemento
 function addInvalid(element) {
   element.classList.remove("is-valid");
   element.classList.add("is-invalid");
 }
+// añade clase is-valid a elemento
 function addValid(element) {
   element.classList.remove("is-invalid");
   element.classList.add("is-valid");
 }
-
+// Validación de edad
 document.getElementById("validationAge").addEventListener("change", (event) => {
   if (event.target.value < 18) {
     addInvalid(event.target);
